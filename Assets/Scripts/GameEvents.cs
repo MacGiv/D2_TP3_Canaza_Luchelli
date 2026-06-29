af4c59545271e9ece0393a0faa41294e2b0e9246
MacGiv <tomasluchelli.17@gmail.com>
2026-06-29 06:47:38 -0300

    Add gameplay events script

---
+++ Assets/Scripts/GameEvents.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public struct PlayerHealthChangedEvent
+{
+    public int CurrentHealth;
+    public int MaxHealth;
+
+    public PlayerHealthChangedEvent(int currentHealth, int maxHealth)
+    {
+        CurrentHealth = currentHealth;
+        MaxHealth = maxHealth;
+    }
+}
+
+public struct SoulCollectedEvent
+{
+    public int Amount;
+    public SoulCollectedEvent(int amount) => Amount = amount;
+}
+
+public struct UIUpdateSoulsEvent
+{
+    public int TotalSouls;
+    public UIUpdateSoulsEvent(int totalSouls) => TotalSouls = totalSouls;
+}
+
+public struct TutorialToggleEvent
+{
+    public string Message;
+    public Sprite TutorialSprite;
+    public bool Show;
+
+    public TutorialToggleEvent(string message, Sprite tutorialSprite, bool show)
+    {
+        Message = message;
+        TutorialSprite = tutorialSprite;
+        Show = show;
+    }
+}
